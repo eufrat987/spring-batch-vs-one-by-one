ec074147ad3a2148f258146f191956a35e41088c
eufrat <lukzyw@gmail.com>
2025-05-15 19:23:48 +0200

    Add note

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class);
+        var context = SpringApplication.run(Main.class);
+
+        // Query to test: update t_person set first_name = ?, last_name = ? where id = ?
+        // We measure here time to complete whole update operation
+        // 1. Update rows one by one
+        // 2. Update rows using batch
     }
 }
